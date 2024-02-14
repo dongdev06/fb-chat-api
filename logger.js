dcba4bc6bbe62ab4f7eec72ba64235c3640a87ff
Phạm Minh Đồng <142565609+dongdev06@users.noreply.github.com>
2024-02-14 15:00:20 +0700

    Update logger.js

--- logger.js
+++ logger.js
@@ -2,12 +2,13 @@
 /* eslint-disable linebreak-style */
 
 const chalk = require('chalk');
-var isHexcolor = require('is-hexcolor');
+
 var getText = function(/** @type {string[]} */ ...Data) {
 	var Main = (Data.splice(0,1)).toString();
-		for (let i = 0; i < Data.length; i++) Main = Main.replace(RegExp(`%${i + 1}`, 'g'), Data[i]);
+	for (let i = 0; i < Data.length; i++) Main = Main.replace(RegExp(`%${i + 1}`, 'g'), Data[i]);
 	return Main;
 };
+
 /**
  * @param {any} obj
  */
@@ -17,50 +18,56 @@ function getType(obj) {
 
 module.exports = {
 	Normal: function(/** @type {string} */ Str, /** @type {() => any} */ Data ,/** @type {() => void} */ Callback) {
-		if (isHexcolor(global.Fca.Require.FastConfig.MainColor) != true) {
-			this.Warning(getText(global.Fca.Require.Language.Index.InvaildMainColor,global.Fca.Require.FastConfig.MainColor),process.exit(0));
+		if (!(/^#[0-9A-F]{6}$/i.test(global.Fca.Require.FastConfig.MainColor))) {
+			this.Warning(getText(global.Fca.Require.Language.Index.InvaildMainColor, global.Fca.Require.FastConfig.MainColor), process.exit(0));
+		} else {
+			console.log(chalk.hex(global.Fca.Require.FastConfig.MainColor).bold(`${global.Fca.Require.FastConfig.MainName || '[ LOGIN-ACCOUNT ]'} > `) + Str);
 		}
-		else console.log(chalk.hex(global.Fca.Require.FastConfig.MainColor).bold(`${global.Fca.Require.FastConfig.MainName || '[ FCA-DONG ]'} > `) + Str);
-		if (getType(Data) == 'Function' || getType(Data) == 'AsyncFunction') {
+		if (getType(Data) === 'Function' || getType(Data) === 'AsyncFunction') {
 			return Data();
 		}
 		if (Data) {
 			return Data;
 		}
-		if (getType(Callback) == 'Function' || getType(Callback) == 'AsyncFunction') {
+		if (getType(Callback) === 'Function' || getType(Callback) === 'AsyncFunction') {
 			Callback();
+		} else {
+			return Callback;
 		}
-		else return Callback;
 	},
 	Warning: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
-		console.log(chalk.magenta.bold('[ FCA-WARNING ] > ') + chalk.yellow(str));
-		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+		console.log(chalk.magenta.bold('[ LOGIN-ACCOUNT-WARNING ] > ') + chalk.yellow(str));
+		if (getType(callback) === 'Function' || getType(callback) === 'AsyncFunction') {
 			callback();
+		} else {
+			return callback;
 		}
-		else return callback;
 	},
 	Error: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
 		if (!str) {
-			console.log(chalk.magenta.bold('[ FCA-ERROR ] > ') + chalk.red("Already Faulty, Please Contact: Facebook.com/PMD.06"));
+			console.log(chalk.magenta.bold('[ LOGIN-ACCOUNT-ERROR ] > ') + chalk.red("Already Faulty, Please Contact: Facebook.com/PMD.06"));
 		}
-		console.log(chalk.magenta.bold('[ FCA-ERROR ] > ') + chalk.red(str));
-		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+		console.log(chalk.magenta.bold('[ LOGIN-ACCOUNT-ERROR ] > ') + chalk.red(str));
+		if (getType(callback) === 'Function' || getType(callback) === 'AsyncFunction') {
 			callback();
+		} else {
+			return callback;
 		}
-		else return callback;
 	},
 	Success: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
-		console.log(chalk.hex('#00FFFF').bold(`${global.Fca.Require.FastConfig.MainName || '[ FCA-DONG ]'} > `) + chalk.green(str));
-		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+		console.log(chalk.hex('#00FFFF').bold(`${global.Fca.Require.FastConfig.MainName || '[ LOGIN-ACCOUNT ]'} > `) + chalk.green(str));
+		if (getType(callback) === 'Function' || getType(callback) === 'AsyncFunction') {
 			callback();
+		} else {
+			return callback;
 		}
-		else return callback;
 	},
 	Info: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
-		console.log(chalk.hex('#00FFFF').bold(`${global.Fca.Require.FastConfig.MainName || '[ FCA-DONG ]'} > `) + chalk.blue(str));
-		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+		console.log(chalk.hex('#00FFFF').bold(`${global.Fca.Require.FastConfig.MainName || '[ LOGIN-ACCOUNT ]'} > `) + chalk.blue(str));
+		if (getType(callback) === 'Function' || getType(callback) === 'AsyncFunction') {
 			callback();
+		} else {
+			return callback;
 		}
-		else return callback;
 	}
 };
